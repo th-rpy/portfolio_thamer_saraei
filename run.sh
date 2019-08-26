06b43e7ad3cbca54716b47e962af29d032d95848
datcom <thamer.assarray@gmail.com>
2019-08-26 15:02:33 +0100

    Initial commit

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 git add .
 git commit -m "Initial commit"
+python3 manage.py makemigrations
+python3 manage.py migrate
+git push heroku master
+heroku run python3 manage.py makemigrations
+heroku run python3 manage.py migrate
